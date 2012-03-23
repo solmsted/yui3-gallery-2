0fd3c56cea1598261146d647f9ad5ae54e85fe87
Steven Olmsted <steven.olm@gmail.com>
2012-03-22 21:00:49 -0400

    Added API documentation.

---
+++ gallery-qr-code-generator-base/api/api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "QrCode",
+        "QrCode.AlphanumericData",
+        "QrCode.Data",
+        "QrCode.GeneratorBase",
+        "QrCode.NumericData"
+    ],
+    "modules": [
+        "gallery-qr-code-generator-base"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-qr-code-generator-base",
+            "name": "gallery-qr-code-generator-base"
+        }
+    ]
+} };
+});
